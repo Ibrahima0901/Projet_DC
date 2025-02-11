d97994ecb4bf141c5974b2b62b6d6d3e07ff14c7
Ibrahima0901 <iboug670@gmail.com>
2025-02-11 13:47:53 +0000

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -14,8 +14,6 @@ import plotly.express as px
 
 # Initialisation des options du navigateur Selenium
 options = Options()
-chromedriver --whitelisted-ips=""
-
 options.add_argument("--whitelisted-ips")#Autorise toutes les IPs (à restreindre si possible)
 options.add_argument("--headless")  # Mode sans interface graphique
 options.add_argument("--disable-gpu")  # Evite certains bugs d'affichage
